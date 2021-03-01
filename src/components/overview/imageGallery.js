a52bebda775b38801141e31d79b1ccd287bee259
Emma <knoremma@gmaill.com>
2021-02-28 21:25:00 -0700

    Add components for overview

---
+++ src/components/overview/imageGallery.js
@@ -0,0 +1,11 @@
+import './ov.css';
+import React from 'react';
+
+
+function ImageGallery() {
+  return (
+    <div className="image-gallery"> </div>
+  );
+}
+
+export default ImageGallery;
---
+++ src/components/overview/styleSelector.js
@@ -0,0 +1,14 @@
+import './ov.css';
+import React from 'react';
+import ImageGallery from './imageGallery.js';
+import ProductInfo from './addToCart.js';
+import AddToCart from './addToCart.js';
+
+
+function StyleSelector() {
+  return (
+    <div className="style-selector"> </div>
+  );
+}
+
+export default StyleSelector;
