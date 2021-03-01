4a9544867882f3ddc6e1d5cb85e9f259c104cea1
Emma <knoremma@gmaill.com>
2021-03-01 14:11:52 -0700

    Add controller api get req function & change manifest.json

--- backend/models/overview.js
+++ backend/models/overview.js
@@ -4,10 +4,12 @@ const axios = require('axios')
 const apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/'
 
 let queries = {
-    getProducts: function (callback) {
+    // get a given product based on id - used for product information component
+    getProducts: function (id, callback) {
         let options = {
             method:'get',
-            url: apiUrl+'products',
+            // add product id
+            url: apiUrl+'products'+id,
             headers: {
             'User-Agent': 'request',
             'Authorization': `${config.TOKEN}`,
@@ -15,14 +17,32 @@ let queries = {
             'Connection': 'keep-alive'
             }
         };
-
+        //console.log(options.url);
         axios(options).then((response) => {
             callback(response.data)
-
         }).catch( (error) => {
             console.log(error)
         }
         )
+    },
+    // get a given products style GET /products/:product_id/styles
+    // used for product information component, style selector component, add to cart component, & image gallery component
+    getStyles: function (id, callback) {
+        let options = {
+            method: 'get',
+            url: apiUrl+'products'+id+'styles',
+            headers: {
+            'User-Agent': 'request',
+            'Authorization': `${config.TOKEN}`,
+            'Content-Type': `application/json`,
+            'Connection': 'keep-alive'
+            }
+        };
+        axios(options).then((response) => {
+            callback(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
     }
 }
 
