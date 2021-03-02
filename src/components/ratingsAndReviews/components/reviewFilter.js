d544e667c083c7c2ff32be59e2c6cf25b3c7a03a
Taylor Smart <taylorsmart@gmail.com>
2021-03-02 15:35:27 -0700

    Adjusting component structure, adding bootstrap

---
+++ src/components/ratingsAndReviews/components/reviewFilter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ReviewFilter() {
+  // console.log(props)
+  return (
+    <div className="review-filter">
+      review-filter
+    </div>
+  );
+}
+
+ReviewFilter.propTypes = {
+};
+
+export default ReviewFilter;
--- src/components/ratingsAndReviews/rrMain.js
+++ src/components/ratingsAndReviews/rrMain.js
@@ -1,7 +1,14 @@
 import '../localStyles/rr.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {
+  Navbar, Container, Row, Col, Grid,
+} from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReviewListContainer from './components/reviewListContainer';
+import SummaryContainer from './components/summaryContainer';
+import ReviewFilter from './components/reviewFilter';
 
 function RatingsAndReviews() {
   const [reviewList, setReviewList] = useState([]);
@@ -22,9 +29,17 @@ function RatingsAndReviews() {
   }, []);
 
   return (
-    <div className="rating-review-container">
-      <ReviewListContainer reviewList={reviewList} />
-    </div>
+      <Container>
+      <Row className="rating-review-container">
+          <Col xs={6} md={4}>
+            <SummaryContainer />
+          </Col>
+          <Col xs={6} md={8}>
+            <ReviewFilter className="review-filter" />
+            <ReviewListContainer className="container" reviewList={reviewList} />
+          </Col>
+        </Row>
+      </Container>
   );
 }
 
