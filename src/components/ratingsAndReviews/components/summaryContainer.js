9857f63a8db93d24e752258d543d40810e86cd69
Taylor Smart <taylorsmart@gmail.com>
2021-03-03 12:25:30 -0700

    star logic is working!

--- src/components/ratingsAndReviews/components/summaryContainer.js
+++ src/components/ratingsAndReviews/components/summaryContainer.js
@@ -14,6 +14,47 @@ function SummaryContainer({ meta }) {
     starList.push(<SummaryStar key={keys[i]} name={keys[i]} count={ratings[keys[i]]} />);
   }
 
+  let remainder = 0;
+  remainder = ratingAvg - remainder;
+  let starArr = [];
+  for (let j = 0; j < 5; j++) {
+    console.log('ratingAvg: ', ratingAvg)
+    console.log('Remainder: ', remainder)
+    switch (remainder) { //4.25
+      case 0.25:
+        starArr.push(0.25);
+        remainder -= 0.25;
+        console.log('adding .25: ', remainder)
+        break;
+      case 0.50:
+        starArr.push(0.50);
+        remainder -= 0.50;
+        console.log('adding .50: ', remainder)
+        break;
+      case 0.75:
+        starArr.push(0.75);
+        remainder -= 0.75;
+        console.log('adding .75: ', remainder)
+        break;
+      case 1:
+        starArr.push(1);
+        remainder -= 1;
+        console.log('adding 1: ', remainder)
+        break;
+      case 0:
+        starArr.push(0);
+        console.log('adding 0: ', remainder)
+        break;
+      default:
+        starArr.push(1);
+        remainder -= 1;
+        console.log('adding 1: ', remainder)
+        break;
+    }
+  }
+  console.log('starArr');
+  console.log(starArr);
+
   return (
     <div className="summary-container">
       <h3>Summary Container</h3>
