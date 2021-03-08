9dee7c8c8af1b08c730603f3f6f6dbe730f1e203
Emma <knoremma@gmaill.com>
2021-03-07 23:29:09 -0700

    Add scrollability to thumbnails

--- src/components/overview/components/imageGallery.js
+++ src/components/overview/components/imageGallery.js
@@ -9,11 +9,13 @@ function ImageGallery({ selected, currentIndex, resetIndex }) {
   const [length, setLength] = useState(0);
   const [currentThumbnail, setCurrentThumbnail] = useState(0);
   const [expanded, setExpanded] = useState(false);
+  const [displayThumbnails, setDisplayThumbnails] = useState([]);
 
 
   // Set the length to match current children from props
   useEffect(() => {
     selected && setLength(selected.photos.length);
+    setDisplayThumbnails([0,1,2,3,4,5,6]);
   }, [selected]);
 
   const next = () => currentIndex < (length - 1) && resetIndex(currentIndex + 1);
@@ -24,18 +26,39 @@ function ImageGallery({ selected, currentIndex, resetIndex }) {
 
   const setCollapsed = () => setExpanded(false);
 
+  const down = () => {
+    let downArr = [...displayThumbnails];
+    if (downArr[downArr.length - 1] !== selected.photos.length - 1) {
+      downArr = downArr.slice(1);
+      downArr.push(downArr[downArr.length - 1] + 1);
+      setDisplayThumbnails(downArr);
+    }
+  };
+
+  const up  = () => {
+    let upArr = [...displayThumbnails];
+    if (upArr[0] !== 0) {
+      upArr.pop();
+      upArr.unshift(upArr[0] - 1);
+      setDisplayThumbnails(upArr);
+    }
+  };
+
   return (
     // pure css carousel
     <React.Fragment>
       <ExpandedCarousel show={expanded} expanded={expanded} selected={selected} currentIndex={currentIndex} onHide={setCollapsed} setExpandedState={setExpandedState} setCollapsed={setCollapsed}/>
       <div className="carousel-container">
         { currentIndex > 0 && <button className="left-arrow" onClick={prev}>&lt;</button> }
-        { selected.photos.length > 7 && currentThumbnail > 6 ? <button className="up-arrow">&and;</button> : null }
+        { selected.photos.length > 7 && displayThumbnails[displayThumbnails.length - 1] > 6 ? <button className="up-arrow" onClick={up}>&and;</button> : null }
           <div className="carousel-wrapper">
             { selected &&
               <div className="thumbnail-carousel">
                 { selected.photos.map((thumbnail, i) => (
-                  <img key={i} className={currentIndex === i ? "selected-thumbnail" : "thumbnail-carousel-img"} src={thumbnail.thumbnail_url} onClick={() => {resetIndex(i)}}/>
+                  <React.Fragment>
+                    {displayThumbnails.includes(i) && <img key={i} className={currentIndex === i ? "selected-thumbnail" : "thumbnail-carousel-img"} src={thumbnail.thumbnail_url} onClick={() => {resetIndex(i)}}/>}
+                    {/* // <img key={i} className={currentIndex === i ? "selected-thumbnail" : "thumbnail-carousel-img"} src={thumbnail.thumbnail_url} onClick={() => {resetIndex(i)}}/> */}
+                  </React.Fragment>
                 )) }
               </div> }
               <div className="carousel-content-wrapper">
@@ -48,7 +71,7 @@ function ImageGallery({ selected, currentIndex, resetIndex }) {
                   </div>
               </div>
           </div>
-        { selected.photos.length > 7 && currentThumbnail < 6 ? <button className="down-arrow">&or;</button> : null }
+        { selected.photos.length > 7 && displayThumbnails[displayThumbnails.length - 1] !== selected.photos.length - 1 ? <button className="down-arrow" onClick={down}>&or;</button> : null }
         { currentIndex < (length - 1) && <button className="right-arrow" onClick={next}>&gt;</button> }
         </div>
     </React.Fragment>
