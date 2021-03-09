7e489dec7e5256daaa5becb3d9beb81f463f7cb7
Ciaddie <ciaddiebb@gmail.com>
2021-03-09 16:20:36 -0700

    working on rendering one question

--- src/components/questionsAndAnswers/QuestionsList.js
+++ src/components/questionsAndAnswers/QuestionsList.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Question from './Question.js';
 'react-bootstrap';
+import axios from 'axios';
 import { Navbar, Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const QuestionList =  (props) => (
+function QuestionList(){
+  useEffect(() => {
+    axios.get('http://localhost3000/qa/questions')
+    console.log('IN THE USE EFFECT')
+    .then((results) => {
+      console.log('USE EFFECT SUCCESS')
+      console.log('THESE ARE RESULTS',results);
+    })
+    .catch((err) => {
+      console.log('USE EFFECT FAILS');
+      console.log(err);
+    });
+  }, []);
       // <div>
       //   {console.log(props.question)}
       //   {props.question.map((question, i) => <Question question={question} key={question+1}/>)}
       // </div>
-      <Container key="summary-inside" className="container border-primary question-container">
-        <div className="border border-secondary shadow">
-          <h5>fill with questions here</h5>
-        </div>
-      </Container>
-)
+      return(<Container key="summary-inside" className="container border-primary question-container">
+      <div className="border border-secondary shadow">
+        <h5>fill with questions here</h5>
+      </div>
+    </Container>)
+
+}
 
 export default QuestionList;
