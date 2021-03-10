9ad77c2199da99503ddd18c1860a50ed9c62fe0d
Emma <knoremma@gmaill.com>
2021-03-10 10:15:56 -0700

    Add snapshot test

--- src/_tests_/overview/overview.test.js
+++ src/_tests_/overview/overview.test.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import Enzyme, { shallow, mount, render } from 'enzyme';
+import renderer from 'react-test-renderer';
 import axios from 'axios';
 import { ContactContextProvider } from '../../Global-Context';
 import { product, style } from '../testData';
@@ -10,6 +11,15 @@ import ImageGallery from '../../components/overview/components/imageGallery';
 import ProductInfo from '../../components/overview/components/productInfo';
 import StyleSelector from '../../components/overview/components/styleSelector';
 import AddToCart from '../../components/overview/components/addToCart';
+
+it('matches the overview snapshot', () => {
+  const snapshot = renderer
+    .create(<ContactContextProvider><Overview /></ContactContextProvider>)
+    .toJSON();
+  expect(snapshot).toMatchSnapshot();
+});
+
+// npm test -- -u to update a snapshot
 
 jest.mock('axios');
 
@@ -21,17 +31,19 @@ const getInfo = () => Promise.all([
 describe('Overview', () => {
   let wrapper;
 
-  it('should render', () => {
+  it('should render without props', () => {
     wrapper = shallow(<ContactContextProvider><Overview getInfo={getInfo} /></ContactContextProvider>);
-    expect(wrapper.exists()).toBe(true);
+    // expect(wrapper.exists()).toBe(true);
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('should have image gallery', () => {
+    wrapper = shallow(<ContactContextProvider><Overview getInfo={getInfo} /></ContactContextProvider>);
     expect(wrapper.find(<ImageGallery />)).toBeTruthy();
   });
 
   it('should have product info', () => {
-    expect(wrapper.find(<ProductInfo />)).toBeTruthy();
+    expect(wrapper.find(<ProductInfo styles={style} />)).toBeTruthy();
   });
 
   it('should have style selector', () => {
@@ -39,7 +51,7 @@ describe('Overview', () => {
   });
 
   it('should have add to cart', () => {
-    expect(wrapper.find(<AddToCart />)).toBeTruthy();
+    expect(wrapper.find(<AddToCart selected={{}} />)).toBeTruthy();
   });
 
   // it('should conditionally render child components', () => {
